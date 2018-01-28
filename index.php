844dd9737ce459a2e270217747dc7796588d3020
Jimmy <jimmy_onion@hotmail.com>
2018-01-28 22:04:33 +0000

    Added table functionality to index.php

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
-<pre>
 <?php
   $db = new PDO('mysql:host=localhost;dbname=warframe;charset=utf8mb4', 'root', 'root');
-  $stmt = $db->prepare("SELECT * FROM primary_weapons WHERE pw_id=:id");
-  $stmt->execute(array(':id' => 1));
+  $stmt = $db->prepare("SELECT * FROM primary_weapons");
+  $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  print_r($rows);
+  // print_r($rows);
 
-  // foreach ($rows as $row) {
-  //   print_r($row['']);
-  // }
+  echo "<table>";
+
+  foreach ($rows as $row) {
+    echo "<tr>";
+      echo "<td>" . $row['pw_id'] . "</td>";
+      echo "<td>" . $row['pw_name'] . "</td>";
+
+      echo "<td>";
+        if($row['pw_owned'] == 0) {
+          echo "No";
+        } else {
+          echo "Yes";
+        }
+      echo "</td>";
+
+    echo "</tr>";
+  }
+
+  echo "</table>";
+
 ?>
