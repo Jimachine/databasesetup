674ee95c92b2eed6a5d2a5f361e726e2367aff39
Jimmy <jimmy_onion@hotmail.com>
2018-01-29 20:30:20 +0000

    added bootstrap styles

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Changa+One|Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/responsive.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warframe Database</title>
   </head>
@@ -23,11 +24,18 @@
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
   // print_r($rows);
 
-  echo "<table>";
+
+  echo '<table class="table table-dark">';
+
+  echo "<thead>";
+    echo "<tr>";
+      echo "<th>ID</th>";
+      echo "<th>Name</th>";
+      echo "<th>Owned</th>";
+    echo "</tr>";
+  echo "</thead>";
 
   foreach ($rows as $row) {
     echo "<tr>";
