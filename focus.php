2b5fe1a024e5976ce972439889482fe427908ee7
Jimmy <jimmy_onion@hotmail.com>
2018-02-11 21:13:23 +0000

    added focus page but not added anything to it yet, broke navbar

---
+++ focus.php
@@ -0,0 +1,20 @@
+<?php include "incs/header.php"; ?>
+<?php include "incs/footer.php"; ?>
+
+<body>
+  <div class="talents">
+
+    <div data-talent="talent1" class="talent">
+      <p>1</p>
+    </div>
+
+    <div data-talent="talent2" class="talent">
+      <p>2</p>
+    </div>
+
+    <div data-talent="talent3" class="talent">
+      <p>3</p>
+    </div>
+
+  </div>
+</body>
